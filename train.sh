ef0656ea7625463fd4727374c00cd7ab647a8948
loeaf <ckdic8524@naver.com>
2024-12-03 14:35:38 +0900

    init

---
+++ train.sh
@@ -0,0 +1,1 @@
+ 1853  nohup python train.py --experiment_dir=/data/dataset --batch_size=220 --lr=0.001 --epoch=1000 --sample_steps=100 --schedule=20 --L1_penalty=100 --Lconst_penalty=15 --embedding_num=704 --gpu_ids 0 1 &
